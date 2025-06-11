15160af13b1ce1fc81c1cecc23344984102c4015
Yuvika Kathaith <139555240+yuvikaKathaith@users.noreply.github.com>
2025-06-11 22:44:33 +0530

    circularLL

--- LinkedList/circular.cpp
+++ LinkedList/circular.cpp
@@ -1,107 +1,121 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Node{
-    public:
+
+class Node {
+public:
     int data;
-    Node*next;
-    //constructor
-    Node(int data){
-        this -> data = data;
-        this -> next = NULL;
-    }
-    ~Node(){
-        int value=this->data;
-        // memory free
-        if(this->next!=NULL){
-            delete this;
-            this->next=NULL;
-        }
+    Node* next;
 
+    Node(int data) {
+        this->data = data;
+        this->next = nullptr;
     }
-};    
-void print(Node*&tail){
-    if(tail==NULL){
-        cout<<"list is empty";
+
+    ~Node() {
+        int value = this->data;
+        if (this->next != nullptr) {
+            this->next = nullptr;
+        }
+        cout << "Memory freed for node with data " << value << endl;
+    }
+};
+
+void print(Node* tail) {
+    if (tail == nullptr) {
+        cout << "List is empty" << endl;
         return;
     }
-    Node*temp=tail;
-    do{
-        cout<<temp->data<<" ";
-        temp=temp->next;
-    }
-    while(temp!=tail);
-    cout<<endl;
+
+    Node* temp = tail;
+    do {
+        cout << temp->data << " ";
+        temp = temp->next;
+    } while (temp != tail);
+    cout << endl;
 }
-void insertNode(Node*&tail,int element,int data){
-    //if list is empty
-    if(tail==NULL){
-        Node*temp=new Node(data);
-        tail=temp;
-        temp->next=temp;
-    }
-    //if list is non empty
-    else{
-        //traversing the list and finding the element after which the new node is to put
-        Node*curr=tail;
-        while(curr->data!=element){
-            curr=curr->next;
-        }
-        //element found
-        Node*temp=new Node(data);
-        temp->next=curr->next;
-        curr->next=temp;
-        
-    }  
-}
-void deleteNode(Node*&tail,int value){
-    //list is empty
-    if(tail==NULL){
+
+void insertNode(Node*& tail, int element, int data) {
+    if (tail == nullptr) {
+        Node* newNode = new Node(data);
+        tail = newNode;
+        newNode->next = newNode;
         return;
     }
-    //middle node and last node delete
-    else{
-        Node*prev=tail;
-        Node*curr=prev->next;
-        while(curr->data!=value){
-            prev = curr;
-            curr=curr->next;
-        }
-        //1 node LL
-        if(curr==prev){
-            tail=NULL;
-        }
 
-        //>=2 node LL
-        prev -> next = curr -> next;
-        if(tail==curr){
-            tail=prev;
+    Node* curr = tail;
+    do {
+        if (curr->data == element) {
+            Node* newNode = new Node(data);
+            newNode->next = curr->next;
+            curr->next = newNode;
+            if (curr == tail) {
+                tail = newNode;
+            }
+            return;
         }
-        curr -> next = NULL;
-        delete curr;
-    }
+        curr = curr->next;
+    } while (curr != tail);
+
+    cout << "Element " << element << " not found in the list." << endl;
 }
-int main(){
-    Node*tail=NULL;
 
-    //insertion
-    insertNode(tail,1,11);
-    print(tail);
-    insertNode(tail,11,12);
-    print(tail);
-    insertNode(tail,12,13);
-    print(tail);
-    insertNode(tail,11,14);
+void deleteNode(Node*& tail, int value) {
+    if (tail == nullptr) {
+        cout << "List is empty. Nothing to delete." << endl;
+        return;
+    }
+
+    Node* prev = tail;
+    Node* curr = tail->next;
+
+    while (curr->data != value) {
+        if (curr == tail) {
+            cout << "Node with value " << value << " not found." << endl;
+            return;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+
+    if (curr == prev) {
+        tail = nullptr;
+    } else {
+        prev->next = curr->next;
+        if (curr == tail) {
+            tail = prev;
+        }
+    }
+
+    curr->next = nullptr;
+    delete curr;
+}
+
+int main() {
+    Node* tail = nullptr;
+
+    insertNode(tail, 1, 11);
     print(tail);
 
-    //deletion
-    deleteNode(tail,13);
-    print (tail);
-    deleteNode(tail,14);
-    print (tail);  
-    deleteNode(tail,12);
-    print (tail);    
-    deleteNode(tail,11);
-    print (tail);  
+    insertNode(tail, 11, 12);
+    print(tail);
+
+    insertNode(tail, 12, 13);
+    print(tail);
+
+    insertNode(tail, 11, 14);
+    print(tail);
+
+    deleteNode(tail, 13);
+    print(tail);
+
+    deleteNode(tail, 14);
+    print(tail);
+
+    deleteNode(tail, 12);
+    print(tail);
+
+    deleteNode(tail, 11);
+    print(tail);
 
     return 0;
 }
